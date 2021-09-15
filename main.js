e74692e558870e793f9a3cbcede91b79af88d2f3
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-09-15 10:38:34 -0300

    Adicionando ícones do iconmoon

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const menuLista = document.querySelector("nav ul")
-console.log(menuLista)
+const menuIcon = document.querySelector(".icon-menu")
+
+menuIcon.addEventListener("click", exibirMenu)
+
+function exibirMenu(){
+    if(menuLista.style.display == "none"){
+        menuLista.style.display = "block"
+    }
+    else{
+        menuLista.style.display = "none"
+    }
+}
