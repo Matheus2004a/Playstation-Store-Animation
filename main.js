c2c48aa1f475441cd3f21e306da8efc41e2d20b7
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-09-17 20:55:25 -0300

    Adicionando modal da conta de usuário

--- main.js
+++ main.js
@@ -11,3 +11,26 @@ function exibirMenu(){
         menuLista.style.display = "none"
     }
 }
+
+const modalCar = document.querySelector(".background-modal-car")
+const modalAccount = document.querySelector(".background-modal-account")
+const iconCar = document.querySelector(".icon-car")
+const iconAccount = document.querySelector(".icon-person")
+const iconCloseModalCar = document.querySelector(".icon-close")
+const iconCloseModalAccount = document.querySelector(".icon-close-account")
+
+iconCar.addEventListener("mouseover", function(){
+    modalCar.style.display = "block"
+})
+
+iconCloseModalCar.addEventListener("click", function(){
+    modalCar.style.display = "none"
+})
+
+iconCloseModalAccount.addEventListener("click", function(){
+    modalAccount.style.display = "none"
+})
+
+iconAccount.addEventListener("mouseover", function(){
+    modalAccount.style.display = "block"
+})
